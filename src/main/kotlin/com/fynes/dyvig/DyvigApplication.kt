493e6479086e9afd3c80c215ce73b0e9838a7c1c
Michael Fynes <mikefynes@gmail.com>
2018-08-08 21:37:25 +0300

    initial commit of structure

---
+++ src/main/kotlin/com/fynes/dyvig/DyvigApplication.kt
@@ -0,0 +1,12 @@
+package com.fynes.dyvig
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DyvigApplication
+
+
+fun main(args: Array<String>) {
+    runApplication<DyvigApplication>(*args)
+}
